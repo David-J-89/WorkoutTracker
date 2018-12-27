2570a88e43cadbe06972d7b5e916570c7572f723
David Whaley <davidwhaleycode@outlook.com>
2018-12-27 12:34:30 -0600

    working on the angular, incomplete

--- WorkoutTracker/WorkoutTracker/Frontend/JoggingDiary/src/app/app.module.ts
+++ WorkoutTracker/WorkoutTracker/Frontend/JoggingDiary/src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { WorkoutService } from './workout.service';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GridJoggingComponent } from './grid-jogging/grid-jogging.component';
+import { AddOrUpdateJoggingComponent } from './add-or-update-jogging/add-or-update-jogging.component';
+import { DecimalPipe } from '@angular/common';
+import { DatePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent }
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GridJoggingComponent,
+    AddOrUpdateJoggingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes),
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    WorkoutService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
