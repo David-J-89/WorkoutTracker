7ec8878fe52eb51a333b45d73a291f8509f4b4b4
David Whaley <davidwhaleycode@outlook.com>
2018-12-27 01:39:09 -0600

    enable cors in configure services method

--- WorkoutTracker/WorkoutTracker/Startup.cs
+++ WorkoutTracker/WorkoutTracker/Startup.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Microsoft.EntityFrameworkCore;
 using WorkoutTracker.Models;
 
 namespace WorkoutTracker
@@ -29,6 +23,15 @@ namespace WorkoutTracker
 
             services.AddDbContext<WorkoutContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("WorkoutContext")));
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
